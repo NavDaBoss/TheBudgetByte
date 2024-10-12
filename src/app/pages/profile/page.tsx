74f7cd6b262e91478d2b2569d368e8ef1ee38c07
nav <nksriniv@ucsc.edu>
2024-10-11 20:46:29 -0700

    template in place for the diff pages

---
+++ src/app/pages/profile/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import React from 'react';
+
+export default function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>Hello, user!</p>
+    </div>
+  );
+}
