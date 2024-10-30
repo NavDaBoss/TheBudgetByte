4d91164786e2a8fc4b0efe23b799fcb918b48961
NavDaBoss <navs4all@gmail.com>
2024-10-30 14:44:31 -0700

    implemented better register and added navbar comp to login as well

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -10,6 +10,8 @@ import {
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 import './register.css';
+import Image from 'next/image';
+import Navbar from '../components/Navbar';
 
 export default function Register() {
   const [email, setEmail] = useState('');
@@ -49,48 +51,89 @@ export default function Register() {
   };
 
   return (
-    <div>
+    <div className="main">
+      <Navbar />
       <h1>Register</h1>
-      <h1>Display Name:</h1>
-      <input
-        type="text"
-        placeholder="Display Name"
-        value={displayName}
-        required
-        onChange={(e) => setDisplayName(e.target.value)}
-      />
-      <h1>Email: </h1>
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        required
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <h1>Password: </h1>
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        autoComplete="new-password"
-        required
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <h1>Confirm Password: </h1>
-      <input
-        type="password"
-        placeholder="Confirm Password"
-        value={confirmPassword}
-        autoComplete="off"
-        required
-        onChange={(e) => setconfirmPassword(e.target.value)}
-      />
-      <button onClick={register}>Register</button>
+      <div className="title-line"></div>
+      <div className="input-container">
+        <Image
+          src="/assets/display_name_icon.svg"
+          alt="Display Name Icon"
+          width={20}
+          height={22.5}
+          className="display-name-icon"
+        />
+        <input
+          type="text"
+          placeholder="Display Name"
+          value={displayName}
+          required
+          onChange={(e) => setDisplayName(e.target.value)}
+          className="display-name-input"
+        />
+      </div>
+      <div className="input-container">
+        <Image
+          src="/assets/email_icon.svg"
+          alt="Email Icon"
+          width={20}
+          height={22.5}
+          className="email-icon"
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          required
+          onChange={(e) => setEmail(e.target.value)}
+          className="email-input"
+        />
+      </div>
+      <div className="input-container">
+        <Image
+          src="/assets/lock_icon.svg"
+          alt="Lock Icon"
+          width={20}
+          height={22.5}
+          className="lock-icon"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          autoComplete="new-password"
+          required
+          onChange={(e) => setPassword(e.target.value)}
+          className="password-input"
+        />
+      </div>
+      <div className="input-container">
+        <Image
+          src="/assets/lock_icon.svg"
+          alt="Lock Icon"
+          width={20}
+          height={22.5}
+          className="lock-icon"
+        />
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={confirmPassword}
+          autoComplete="off"
+          required
+          onChange={(e) => setconfirmPassword(e.target.value)}
+          className="password-input"
+        />
+      </div>
+      <button onClick={register} className="register">Register</button>
       {errorMessage && <p>{errorMessage}</p>}
-      <h1>Already have an account?</h1>
-      <Link href="/login">
-        <button>Continue to Login</button>
-      </Link>
+      <div className="title-line"></div>
+      <div className="login-container">
+        <p>Already have an account?</p>
+        <Link href="/login" className="login-link">
+          Continue to Login
+        </Link>
+      </div>
     </div>
   );
 }
