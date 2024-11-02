e4924b9cb20848d921eded1f0eccd25608f36254
jlin216 <jlin216@ucsc.edu>
2024-11-02 16:05:41 -0700

    added tests

--- jest.setup.js
+++ jest.setup.js
@@ -1,11 +1,11 @@
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 import 'jest-canvas-mock';
 
-import { jest } from "@jest/globals";
-jest.mock("next/navigation", () => ({
-    useRouter() {
-        return {
-            prefetch: () => null
-        };
-    }
+import { jest } from '@jest/globals';
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      prefetch: () => null,
+    };
+  },
 }));
