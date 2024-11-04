9438a1cdff9b8d262ca59d20a03bb6ddbb7ed720
jlin216 <jlin216@ucsc.edu>
2024-11-03 19:05:15 -0800

    added firebase mocks

--- jest.setup.js
+++ jest.setup.js
@@ -9,3 +9,34 @@ jest.mock('next/navigation', () => ({
     };
   },
 }));
+
+// Mock Firebase modules
+const mockApp = {};
+
+jest.mock('firebase/app', () => ({
+  initializeApp: jest.fn(() => mockApp),
+  getApp: jest.fn(() => mockApp),
+}));
+
+jest.mock('firebase/auth', () => ({
+  getAuth: jest.fn(() => ({
+    currentUser: { uid: 'mock-uid', displayName: 'Mock User' },
+    signInWithPopup: jest.fn(),
+    signOut: jest.fn(),
+    onAuthStateChanged: jest.fn((callback) => callback({ uid: 'mock-uid', displayName: 'Mock User' })),
+  })),
+  GoogleAuthProvider: jest.fn(),
+}));
+
+jest.mock('firebase/firestore', () => ({
+  getFirestore: jest.fn(() => ({
+    collection: jest.fn(),
+    doc: jest.fn(),
+    getDocs: jest.fn(),
+    addDoc: jest.fn(),
+  })),
+}));
+
+jest.mock('firebase/storage', () => ({
+  getStorage: jest.fn(() => ({})),
+}));
