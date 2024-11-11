ba4eb9de2fc3bfbd935bec5084e1a24051b74b7d
Ethan Byeon <ethanbyeon+github@gmail.com>
2024-11-10 18:13:50 -0800

    refactor: made a separate file for Receipt component

---
+++ src/app/components/Receipt.tsx
@@ -0,0 +1,207 @@
+import React, { useState } from 'react';
+
+import '../styles/Receipt.css';
+
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+
+const ReceiptHead = ({ sortColumn }) => {
+  const [sortField, setSortField] = useState('');
+  const [order, setOrder] = useState('asc');
+
+  const handleSortChange = (accessor) => {
+    let sortOrder = 'asc';
+    if (accessor === sortField) {
+      sortOrder = order === 'asc' ? 'desc' : order === 'desc' ? 'none' : 'asc';
+    }
+    setSortField(accessor);
+    setOrder(sortOrder);
+    sortColumn(accessor, sortOrder);
+  };
+
+  return (
+    <thead>
+      <tr>
+        <th
+          key="quantity"
+          className="quantity-column"
+          onClick={() => handleSortChange('quantity')}
+        >
+          QTY
+          <span className="sort-arrow"></span>
+        </th>
+        <th
+          key="itemName"
+          className="item-name-column"
+          onClick={() => handleSortChange('itemName')}
+        >
+          ITEM
+          <span className="sort-arrow"></span>
+        </th>
+        <th
+          key="group"
+          className="group-column"
+          onClick={() => handleSortChange('group')}
+        >
+          GROUP
+          <span className="sort-arrow"></span>
+        </th>
+        <th
+          key="price"
+          className="price-column"
+          onClick={() => handleSortChange('price')}
+        >
+          PRICE
+          <span className="sort-arrow"></span>
+        </th>
+      </tr>
+    </thead>
+  );
+};
+
+const ReceiptRow = ({ item }) => {
+  return (
+    <tr>
+      <td className="quantity-column">{item.quantity}</td>
+      <td className="item-name-column">{item.itemName}</td>
+      <td className="group-column">{item.group}</td>
+      <td className="price-column">${item.price.toFixed(2)}</td>
+    </tr>
+  );
+};
+
+const ReceiptTable = ({
+  groceries,
+  filterText,
+  sortColumn,
+  page,
+  itemsPerPage,
+}) => {
+  const startIndex = page * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const paginatedGroceries = groceries.slice(startIndex, endIndex);
+
+  const rows = [];
+
+  paginatedGroceries.forEach((item) => {
+    if (item.itemName.toLowerCase().indexOf(filterText.toLowerCase()) === -1) {
+      return;
+    }
+    rows.push(<ReceiptRow item={item} key={item.itemName} />);
+  });
+
+  return (
+    <table className="receipt-table">
+      <ReceiptHead sortColumn={sortColumn} />
+      <tbody className="receipt-body">{rows}</tbody>
+    </table>
+  );
+};
+
+const SearchBar = ({ filterText, onFilterTextChange }) => {
+  return (
+    <div className="search-bar-container">
+      <input
+        className="search-bar"
+        type="text"
+        value={filterText}
+        placeholder="Search..."
+        onChange={(e) => onFilterTextChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+const Receipt = ({ groceries }) => {
+  const [tableData, setTableData] = useState(groceries);
+  const [filterText, setFilterText] = useState('');
+  const [page, setPage] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+
+  const sortColumn = (sortField, sortOrder) => {
+    if (sortOrder === 'none') {
+      setTableData(groceries);
+      return;
+    }
+
+    if (sortField) {
+      const sorted = [...groceries].sort((a, b) => {
+        if (typeof a[sortField] === 'number') {
+          return (a[sortField] - b[sortField]) * (sortOrder === 'asc' ? 1 : -1);
+        }
+        a = a[sortField].toLowerCase();
+        b = b[sortField].toLowerCase();
+
+        return (
+          a.localeCompare(b, 'en', {
+            numeric: true,
+          }) * (sortOrder === 'asc' ? 1 : -1)
+        );
+      });
+      setTableData(sorted);
+    }
+  };
+
+  const totalPages = Math.ceil(tableData.length / itemsPerPage);
+
+  const handleNextPage = () => {
+    if (page < totalPages - 1) {
+      setPage(page + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleItemsPerPageChange = (event) => {
+    setItemsPerPage(Number(event.target.value));
+    setPage(0);
+  };
+
+  const startItem = page * itemsPerPage + 1;
+  const endItem = Math.min(startItem + itemsPerPage - 1, tableData.length);
+
+  return (
+    <div>
+      <div className="receipt-head">
+        <h1>Receipt</h1>
+        <SearchBar filterText={filterText} onFilterTextChange={setFilterText} />
+      </div>
+      <ReceiptTable
+        groceries={tableData}
+        filterText={filterText}
+        sortColumn={sortColumn}
+        page={page}
+        itemsPerPage={itemsPerPage}
+      />
+      <div className="pagination-controls">
+        <div className="rows-per-page">
+          <label htmlFor="rowsPerPage">Rows per page:</label>
+          <select
+            id="rowsPerPage"
+            value={itemsPerPage}
+            onChange={handleItemsPerPageChange}
+          >
+            <option value={5}>5</option>
+            <option value={10}>10</option>
+            <option value={20}>20</option>
+          </select>
+        </div>
+        <button onClick={handlePreviousPage} disabled={page === 0}>
+          <KeyboardArrowLeftIcon fontSize="large" />
+        </button>
+        <span>
+          {startItem}-{endItem} of {tableData.length} items
+        </span>
+        <button onClick={handleNextPage} disabled={page === totalPages - 1}>
+          <KeyboardArrowRightIcon fontSize="large" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Receipt;
