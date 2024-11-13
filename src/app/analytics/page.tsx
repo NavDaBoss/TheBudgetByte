181d07f6826760b66d66f9c1be00c23cbbca0795
jlin216 <jlin216@ucsc.edu>
2024-11-12 21:50:54 -0800

    Cleaned up analytics code, isolated yearly graph and summary

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -4,161 +4,44 @@ import React, { useEffect, useState } from 'react';
 import './analytics.css';
 import Navbar from '../components/Navbar';
 import Summary from '../components/Summary';
-import {
-  AnalyticsLineGraph,
-  CategoryLegend,
-  GraphParams,
-} from '../components/YearlyGraph';
-import {
-  FoodTypes,
-  FoodGroupInfo,
-  MonthlyData,
-  YearlyOverview,
-  YearlyOverviewData,
-} from './yearlyOverviewInterface';
+import { YearlyOverview } from './yearlyOverviewInterface';
 import { useRouter } from 'next/navigation';
 import { auth, db } from '../firebase/firebaseConfig';
 import { collection, query, where, getDocs } from 'firebase/firestore';
 import DropDown from '../components/DropdownButton';
+import SpendingInYearGraph, { months } from './spendingInYearGraph';
 
-const months = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-];
-
-// Displays the months before and after
-const getMonthsToDisplay = (monthlyData: { [month: string]: MonthlyData }) => {
-  // Sort the keys of monthlyData according to the `months` array order
-  const sortedMonths = Object.keys(monthlyData).sort(
-    (a, b) => months.indexOf(a) - months.indexOf(b),
+// Uses the summary in dashboard populated with the food data from the selected month out of the selected year.
+const AnalyticsSummary = ({
+  yearlyOverview,
+  selectedYear,
+  selectedMonth,
+}: {
+  yearlyOverview: YearlyOverview | null;
+  selectedYear: string;
+  selectedMonth: string;
+}) => {
+  return (
+    <div className="summary-container">
+      {yearlyOverview &&
+      yearlyOverview.yearlyOverviewData[selectedYear]?.[selectedMonth] ? (
+        <Summary
+          data={
+            yearlyOverview.yearlyOverviewData[selectedYear][selectedMonth]
+              .foodGroups
+          }
+          totalAmount={
+            yearlyOverview.yearlyOverviewData[selectedYear][selectedMonth]
+              .totalSpent
+          }
+        />
+      ) : (
+        <p>
+          No receipts scanned in {selectedMonth} {selectedYear}
+        </p>
+      )}
+    </div>
   );
-
-  // Find the first and last months with data in sortedMonths
-  const firstMonth = sortedMonths[0];
-  const lastMonth = sortedMonths[sortedMonths.length - 1];
-
-  // Get the indices of these months in the full `months` array.
-  let start = months.indexOf(firstMonth);
-  let end = months.indexOf(lastMonth);
-
-  // Display the month before and after the months with data.
-  if (start > 0) {
-    start -= 1;
-  }
-  if (end < months.length - 1) {
-    end += 1;
-  }
-  // Use end + 1 to include last month in the slide.
-  return months.slice(start, end + 1);
-};
-
-const createYearlyMoneySpentGraphParams = (
-  selectedYear: string,
-  yearlyOverview: YearlyOverview,
-  categoryLegend: CategoryLegend,
-  setCategoryLegend: React.Dispatch<React.SetStateAction<CategoryLegend>>,
-) => {
-  // get the data for all populated months of the selected year
-  const monthlyData = yearlyOverview.yearlyOverviewData[selectedYear];
-  if (!monthlyData) {
-    throw new Error(`No data available for ${selectedYear}`);
-  }
-
-  const displayedMonths = getMonthsToDisplay(monthlyData);
-  // Get the total cost spent on a category
-  const getCategoryData = (category: FoodTypes) => {
-    return displayedMonths.map((month) => {
-      if (month in monthlyData) {
-        const foodGroup = monthlyData[month].foodGroups.find(
-          (group: FoodGroupInfo) => group.type === category,
-        );
-        return foodGroup ? foodGroup.totalCost : 0;
-      } else {
-        return 0;
-      }
-    });
-  };
-
-  // Calculate total spending for the visible categories
-  const calculateTotalData = () => {
-    return displayedMonths.map((month) => {
-      let sum = 0;
-      for (const category in categoryLegend) {
-        if (
-          categoryLegend[category] &&
-          category !== 'Total' &&
-          month in monthlyData
-        ) {
-          // Check if category is checkmarked
-          const foodGroup = monthlyData[month].foodGroups.find(
-            (group: FoodGroupInfo) => group.type === category,
-          );
-          sum += foodGroup ? foodGroup.totalCost : 0;
-        }
-      }
-      return sum;
-    });
-  };
-
-  const graphData = {
-    labels: displayedMonths,
-    datasets: [
-      {
-        label: 'Total',
-        data: calculateTotalData(),
-        borderColor: 'rgba(112,128,144, 1)',
-        hidden: !categoryLegend.Total,
-      },
-      {
-        label: FoodTypes.Fruits,
-        data: getCategoryData(FoodTypes.Fruits),
-        borderColor: 'rgba(255,182,193, 1)',
-        hidden: !categoryLegend.Fruits,
-      },
-      {
-        label: FoodTypes.Vegetables,
-        data: getCategoryData(FoodTypes.Vegetables),
-        borderColor: 'rgba(143,188,143, 1)',
-        hidden: !categoryLegend.Vegetables,
-      },
-      {
-        label: FoodTypes.Protein,
-        data: getCategoryData(FoodTypes.Protein),
-        borderColor: 'rgba(147,112,219, 1)',
-        hidden: !categoryLegend.Protein,
-      },
-      {
-        label: FoodTypes.Grains,
-        data: getCategoryData(FoodTypes.Grains),
-        borderColor: 'rgba(255,140,0, 1)',
-        hidden: !categoryLegend.Grains,
-      },
-      {
-        label: FoodTypes.Dairy,
-        data: getCategoryData(FoodTypes.Dairy),
-        fill: false,
-        borderColor: 'rgba(135,206,250, 1)',
-        hidden: !categoryLegend.Dairy,
-      },
-    ],
-  };
-  const graphParams: GraphParams = {
-    categoryLegend,
-    setCategoryLegend,
-    graphData,
-  };
-
-  return graphParams;
 };
 
 const Analytics = () => {
@@ -167,14 +50,6 @@ const Analytics = () => {
   const [yearlyOverview, setOverview] = useState<YearlyOverview | null>(null);
   const [selectedYear, setSelectedYear] = useState('2024');
   const [selectedMonth, setSelectedMonth] = useState('January');
-  const [categoryLegend, setCategoryLegend] = useState<CategoryLegend>({
-    Total: true,
-    Fruits: true,
-    Vegetables: true,
-    Protein: true,
-    Grains: true,
-    Dairy: true,
-  });
 
   useEffect(() => {
     if (!currentUser) {
@@ -194,38 +69,38 @@ const Analytics = () => {
             setOverview(null); // Ensure state is updated to indicate no data
           }
         } catch (error) {
-          console.error('Error fetching receipt count:', error);
+          console.error(
+            'Error fetching yearly overview from firestore:',
+            error,
+          );
         }
       };
       fetchYearlyOverview();
     }
   }, [currentUser, router]);
 
-  // Set initial selectedYear and selectedMonth when yearlyOverview is ready
+  // Set initial selectedYear and selectedMonth when yearlyOverview is ready.
   useEffect(() => {
     if (yearlyOverview) {
-      const years = Object.keys(yearlyOverview.yearlyOverviewData);
-      const initialYear = years[0];
-      const months = Object.keys(
-        yearlyOverview.yearlyOverviewData[initialYear],
+      // Sort the years in descending order (most recent year first).
+      const years = Object.keys(yearlyOverview.yearlyOverviewData).sort(
+        (a, b) => Number(b) - Number(a),
       );
-      const initialMonth = months[0];
+      const initialYear = years[0];
+      // Sort the keys of monthlyData according to the `months` array order (January -> December).
+      const sortedMonths = Object.keys(
+        yearlyOverview.yearlyOverviewData[initialYear],
+      ).sort((a, b) => months.indexOf(a) - months.indexOf(b));
 
       setSelectedYear(initialYear);
-      setSelectedMonth(initialMonth);
+      setSelectedMonth(sortedMonths[0]);
     }
   }, [yearlyOverview]);
-  const graphParams = yearlyOverview
-    ? createYearlyMoneySpentGraphParams(
-        selectedYear,
-        yearlyOverview,
-        categoryLegend,
-        setCategoryLegend,
-      )
-    : null;
+  // Default to 2024 if no yearly overview data exists.
   const yearValues = yearlyOverview
     ? Object.keys(yearlyOverview.yearlyOverviewData)
     : ['2024'];
+
   return (
     <div className="page">
       <div>
@@ -240,16 +115,10 @@ const Analytics = () => {
               values={yearValues}
               drop_label="Selected Year:"
             />
-            <div className="graph-container">
-              {graphParams ? (
-                <AnalyticsLineGraph
-                  selectedYear={selectedYear}
-                  params={graphParams}
-                />
-              ) : (
-                <p>No receipts scanned in {selectedYear}</p>
-              )}
-            </div>
+            <SpendingInYearGraph
+              selectedYear={selectedYear}
+              yearlyOverview={yearlyOverview}
+            />
           </div>
         </div>
         <div className="month-container">
@@ -260,29 +129,11 @@ const Analytics = () => {
               values={months}
               drop_label="Selected Month:"
             />
-            <div className="summary-container">
-              {yearlyOverview &&
-              yearlyOverview.yearlyOverviewData[selectedYear]?.[
-                selectedMonth
-              ] ? (
-                <Summary
-                  data={
-                    yearlyOverview.yearlyOverviewData[selectedYear][
-                      selectedMonth
-                    ].foodGroups
-                  }
-                  totalAmount={
-                    yearlyOverview.yearlyOverviewData[selectedYear][
-                      selectedMonth
-                    ].totalSpent
-                  }
-                />
-              ) : (
-                <p>
-                  No receipts scanned in {selectedMonth} {selectedYear}
-                </p>
-              )}
-            </div>
+            <AnalyticsSummary
+              yearlyOverview={yearlyOverview}
+              selectedYear={selectedYear}
+              selectedMonth={selectedMonth}
+            />
           </div>
         </div>
       </div>
---
+++ src/app/analytics/spendingInYearGraph.tsx
@@ -0,0 +1,189 @@
+import React, { useState } from 'react';
+import './analytics.css';
+import {
+  AnalyticsLineGraph,
+  CategoryLegend,
+  GraphParams,
+} from '../components/YearlyGraph';
+import {
+  FoodTypes,
+  FoodGroupInfo,
+  MonthlyData,
+  YearlyOverview,
+} from './yearlyOverviewInterface';
+export const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+// Displays the months before and after
+const getMonthsToDisplay = (monthlyData: { [month: string]: MonthlyData }) => {
+  // Sort the keys of monthlyData according to the `months` array order
+  const sortedMonths = Object.keys(monthlyData).sort(
+    (a, b) => months.indexOf(a) - months.indexOf(b),
+  );
+
+  // Find the first and last months with data in sortedMonths
+  const firstMonth = sortedMonths[0];
+  const lastMonth = sortedMonths[sortedMonths.length - 1];
+
+  // Get the indices of these months in the full `months` array.
+  let start = months.indexOf(firstMonth);
+  let end = months.indexOf(lastMonth);
+
+  // Display the month before and after the months with data.
+  if (start > 0) {
+    start -= 1;
+  }
+  if (end < months.length - 1) {
+    end += 1;
+  }
+  // Use end + 1 to include last month in the slide.
+  return months.slice(start, end + 1);
+};
+
+const createYearlyMoneySpentGraphParams = (
+  selectedYear: string,
+  yearlyOverview: YearlyOverview,
+  categoryLegend: CategoryLegend,
+  setCategoryLegend: React.Dispatch<React.SetStateAction<CategoryLegend>>,
+) => {
+  // get the data for all populated months of the selected year
+  const monthlyData = yearlyOverview.yearlyOverviewData[selectedYear];
+  if (!monthlyData) {
+    throw new Error(`No data available for ${selectedYear}`);
+  }
+
+  const displayedMonths = getMonthsToDisplay(monthlyData);
+  // Get the total cost spent on a category
+  const getCategoryData = (category: FoodTypes) => {
+    return displayedMonths.map((month) => {
+      if (month in monthlyData) {
+        const foodGroup = monthlyData[month].foodGroups.find(
+          (group: FoodGroupInfo) => group.type === category,
+        );
+        return foodGroup ? foodGroup.totalCost : 0;
+      } else {
+        return 0;
+      }
+    });
+  };
+
+  // Calculate total spending for the visible categories
+  const calculateTotalData = () => {
+    return displayedMonths.map((month) => {
+      let sum = 0;
+      for (const category in categoryLegend) {
+        if (
+          categoryLegend[category] &&
+          category !== 'Total' &&
+          month in monthlyData
+        ) {
+          // Check if category is checkmarked
+          const foodGroup = monthlyData[month].foodGroups.find(
+            (group: FoodGroupInfo) => group.type === category,
+          );
+          sum += foodGroup ? foodGroup.totalCost : 0;
+        }
+      }
+      return sum;
+    });
+  };
+
+  const graphData = {
+    labels: displayedMonths,
+    datasets: [
+      {
+        label: 'Total',
+        data: calculateTotalData(),
+        borderColor: 'rgba(112,128,144, 1)',
+        hidden: !categoryLegend.Total,
+      },
+      {
+        label: FoodTypes.Fruits,
+        data: getCategoryData(FoodTypes.Fruits),
+        borderColor: 'rgba(255,182,193, 1)',
+        hidden: !categoryLegend.Fruits,
+      },
+      {
+        label: FoodTypes.Vegetables,
+        data: getCategoryData(FoodTypes.Vegetables),
+        borderColor: 'rgba(143,188,143, 1)',
+        hidden: !categoryLegend.Vegetables,
+      },
+      {
+        label: FoodTypes.Protein,
+        data: getCategoryData(FoodTypes.Protein),
+        borderColor: 'rgba(147,112,219, 1)',
+        hidden: !categoryLegend.Protein,
+      },
+      {
+        label: FoodTypes.Grains,
+        data: getCategoryData(FoodTypes.Grains),
+        borderColor: 'rgba(255,140,0, 1)',
+        hidden: !categoryLegend.Grains,
+      },
+      {
+        label: FoodTypes.Dairy,
+        data: getCategoryData(FoodTypes.Dairy),
+        fill: false,
+        borderColor: 'rgba(135,206,250, 1)',
+        hidden: !categoryLegend.Dairy,
+      },
+    ],
+  };
+  const graphParams: GraphParams = {
+    categoryLegend,
+    setCategoryLegend,
+    graphData,
+  };
+
+  return graphParams;
+};
+interface SpendingInYearGraphProps {
+  selectedYear: string;
+  yearlyOverview: YearlyOverview | null;
+}
+
+const SpendingInYearGraph: React.FC<SpendingInYearGraphProps> = ({
+  selectedYear,
+  yearlyOverview,
+}) => {
+  const [categoryLegend, setCategoryLegend] = useState<CategoryLegend>({
+    Total: true,
+    Fruits: true,
+    Vegetables: true,
+    Protein: true,
+    Grains: true,
+    Dairy: true,
+  });
+  const graphParams = yearlyOverview
+    ? createYearlyMoneySpentGraphParams(
+        selectedYear,
+        yearlyOverview,
+        categoryLegend,
+        setCategoryLegend,
+      )
+    : null;
+  return (
+    <div className="graph-container">
+      {graphParams ? (
+        <AnalyticsLineGraph selectedYear={selectedYear} params={graphParams} />
+      ) : (
+        <p>No receipts scanned in {selectedYear}</p>
+      )}
+    </div>
+  );
+};
+
+export default SpendingInYearGraph;
