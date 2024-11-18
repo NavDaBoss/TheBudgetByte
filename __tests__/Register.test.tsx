67d8265a0d2563b8193225237b00567fca31b0ce
NavDaBoss <navs4all@gmail.com>
2024-11-17 17:50:56 -0800

    removing unecessary imports from test

--- __tests__/Register.test.tsx
+++ __tests__/Register.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import Register from '@/app/register/page';
 import { useRouter } from 'next/navigation';
 
